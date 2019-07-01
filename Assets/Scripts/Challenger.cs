e3caa4b43942564141de02cfea4abc52cad208da
NIELSPC\Niels <nielsvdam@hotmail.com>
2019-07-01 22:23:14 +0200

    poging

---
+++ Assets/Scripts/Challenger.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Direction
+{
+    North,
+    South,
+    East,
+    West
+}
+
+public class Challenger : MonoBehaviour
+{
+    //DNA
+    public DNA<Direction> dna = null;
+
+    private Vector2 posInMaze;
+    private List<List<int>> maze = null;
+    [SerializeField]private float timeBetweenSteps = 0.1f;
+
+    private Vector3 startWorldPos = Vector3.zero;
+
+    private void Start() {
+        maze = FindObjectOfType<MazeSolver>().maze;
+        startWorldPos = transform.position;
+
+        Initialize();
+    }
+
+    public void Initialize()
+    {
+        posInMaze = new Vector2(1,1);
+        transform.position = startWorldPos;
+        StartCoroutine(Solve());
+    }
+
+    IEnumerator Solve()
+    {
+        foreach(var move in dna.Genes)
+        {
+            if(!ReachedFinish(posInMaze))
+            {
+                yield return new WaitForSeconds(timeBetweenSteps);
+                TryMove(move);
+            }else{
+                
+                print(transform.name + " made it!");
+                print(dna.ToString());
+                DNAManager dnaManager = FindObjectOfType<DNAManager>();
+                dnaManager.BestGenes = dna.Genes;
+                dnaManager.Winner(this);
+                dnaManager.BestFitness = dna.Fitness;
+                break;
+            }
+        }
+
+        ShowResult();
+    }
+
+    void ShowResult()
+    {
+        print(transform.name + " scored " + dna.Fitness.ToString("F2"));
+    }
+
+    void TryMove(Direction dir)
+    {
+        switch (dir)
+        {
+            case Direction.North:
+                if (CanMoveUp(posInMaze))
+                {
+                    posInMaze.x--;
+                    transform.Translate(Vector2.up);
+                }
+                else{
+                    dna.Fitness--;
+                }
+                break;
+            case Direction.South:
+                if (CanMoveDown(posInMaze))
+                {
+                    transform.Translate(Vector2.down);
+                    posInMaze.x++;
+                }
+                else{
+                    dna.Fitness--;
+                }
+                break;
+            case Direction.East:
+                if (CanMoveRight(posInMaze))
+                {
+                    transform.Translate(Vector2.right);
+                    posInMaze.y++;
+                }
+                else{
+                    dna.Fitness--;
+                }
+                break;
+            case Direction.West:
+                if (CanMoveLeft(posInMaze))
+                {
+                    transform.Translate(Vector2.left);
+                    posInMaze.y--;
+                }
+                else{
+                    dna.Fitness--;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    bool ReachedFinish(Vector2 playerPos)
+    {
+        return maze[(int)playerPos.x][(int)playerPos.y] == 2;
+    }
+
+    bool CanMoveRight(Vector2 playerPos)
+    {
+        int nextPos = maze[(int)playerPos.x][(int)playerPos.y + 1];
+        return (nextPos == 0 || nextPos == 2);
+    }
+
+    bool CanMoveLeft(Vector2 playerPos)
+    {
+        int nextPos = maze[(int)playerPos.x][(int)playerPos.y - 1];
+        return (nextPos == 0 || nextPos == 2);
+    }
+
+    bool CanMoveUp(Vector2 playerPos)
+    {
+        int nextPos = maze[(int)playerPos.x - 1][(int)playerPos.y];
+        return (nextPos == 0 || nextPos == 2);
+    }
+
+    bool CanMoveDown(Vector2 playerPos)
+    {
+        int nextPos = maze[(int)playerPos.x + 1][(int)playerPos.y];
+        return (nextPos == 0 || nextPos == 2);
+    }
+}
---
+++ Assets/Scripts/DNAManager.cs
@@ -0,0 +1,149 @@
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class DNAManager : MonoBehaviour 
+{    
+    [Header("Challenger details")]
+    public Challenger challengerPrefab;
+    public int amountOfChallengers = 1;
+    public int genesLength = 14;
+
+    private System.Random random;
+
+    [SerializeField]private List<Challenger> population = new List<Challenger>();
+    public int Generation;
+    public float BestFitness;
+    public Direction[] BestGenes;
+    
+    public float MutationRate;
+
+    private float fitnessSum;
+
+    private void Start() {
+        random = new System.Random();
+        BestGenes = new Direction[genesLength];
+        
+        for (int i = 0; i < amountOfChallengers; i++)
+        {
+            Challenger c = Instantiate(challengerPrefab, challengerPrefab.transform.position, Quaternion.identity);
+            c.dna = new DNA<Direction>(genesLength, random, GetRandomDirection, FitnessFunction, true);
+            c.transform.name = "Challenger " + i;
+            population.Add(c);
+        }
+
+        InvokeRepeating("NewGeneration", 1, 2f);
+    }
+
+
+    public void NewGeneration()
+    {
+        if(population.Count <= 0)
+        {
+            return;
+        }
+
+        CalculateFitness();
+
+        List<Challenger> newPopulation = new List<Challenger>();
+
+        for (int i = 0; i < amountOfChallengers; i++)
+        {
+            Challenger c = Instantiate(challengerPrefab, challengerPrefab.transform.position, Quaternion.identity);
+            c.dna = new DNA<Direction>(genesLength, random, GetRandomDirection, FitnessFunction, true);
+            c.transform.name = "Challenger " + i;
+            newPopulation.Add(c);
+        }
+
+        /* crossovers?? 
+        for (int i = 0; i < population.Count; i++)
+        {
+            DNA<Direction> parent1 = ChooseParent();
+            DNA<Direction> parent2 = ChooseParent();
+            DNA<Direction> child = null;
+
+            Challenger c = Instantiate(challengerPrefab, challengerPrefab.transform.position, Quaternion.identity);
+            if(parent1 != null && parent2 != null)
+            {
+                child = parent1.CrossOver(parent2);
+                child.Mutate(MutationRate);
+                c.dna = child;
+            }else{
+                c.dna = new DNA<Direction>(genesLength, random, GetRandomDirection, FitnessFunction, true);
+            }
+            newPopulation.Add(c);
+        }*/
+        foreach (var item in population)
+        {
+            Destroy(item.gameObject);
+        }
+        population.Clear();
+        population = newPopulation;
+
+        Generation++;
+    }
+
+    public void CalculateFitness()
+    {
+        fitnessSum = 0;
+
+        DNA<Direction> best = population[0].dna;
+
+        for (int i = 0; i < population.Count; i++)
+        {
+            fitnessSum += population[i].dna.CalculateFitness(i);
+
+            if(population[i].dna.Fitness > best.Fitness)
+            {
+                best = population[i].dna;
+            }
+        }
+
+        BestFitness = best.Fitness;
+        best.Genes.CopyTo(BestGenes, 0);
+    }
+
+    private DNA<Direction> ChooseParent()
+    {
+        double randomNumber = random.NextDouble() * fitnessSum;
+
+        for (int i = 0; i < population.Count; i++)
+        {
+            if(randomNumber < population[i].dna.Fitness)
+            {
+                return population[i].dna;
+            }
+
+            randomNumber -= population[i].dna.Fitness;
+        }
+
+        return null;
+    }
+
+    private Direction GetRandomDirection()
+    {
+        int rand = Random.Range(0, 4);
+        return (Direction)rand;
+    }
+
+    private float FitnessFunction(int index)
+    {
+        float score = 0;
+        Challenger challenger = population[index];
+
+        float distToStart = Vector3.Distance(challenger.transform.position, new Vector3(-1.5f, 0.5f, -1.042578f));
+
+        float distToFinish = Vector3.Distance(challenger.transform.position, GameObject.FindGameObjectWithTag("Finish").transform.position);
+
+        score += (distToStart - distToFinish);
+
+        return score;
+    }
+
+    public void Winner(Challenger challenger)
+    {
+        challenger.dna.CalculateFitness(population.IndexOf(challenger));
+        Time.timeScale = 0;
+        this.enabled = false;
+    }
+}
